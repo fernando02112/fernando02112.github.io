e698899005340d761efeb34d5df29773adbbd093
fernando02112 <fernando02112@users.noreply.github.com>
2016-08-16 15:22:29 -0500

    Update ejemplo-2-4.js

--- rv/ejemplo-2-4.js
+++ rv/ejemplo-2-4.js
@@ -1,4 +1,4 @@
 var miCanvas = document.getElementById("miCanvas");
-var contexto = miCanvas.getContext("2D");
+var contexto = miCanvas.getContext("2d");
 contexto.fillStyle="red";
 contexto.fillRect(30,30,50,50);
