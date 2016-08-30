773557df3b7ac53bc59463fa764d2008556946df
fernando02112 <fernando02112@users.noreply.github.com>
2016-08-30 15:13:57 -0500

    Update ejemplo-union.js

--- rv/ejemplo-union.js
+++ rv/ejemplo-union.js
@@ -17,7 +17,7 @@ var escena = new THREE.Scene();
 escena.add(arbolMalla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = .5;
+camara.position.z = 5;
 
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
