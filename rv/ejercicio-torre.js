abbaacd512bfe1b05ba7ad573121d55f43e3418b
fernando02112 <fernando02112@users.noreply.github.com>
2016-09-01 15:16:37 -0500

    Update ejercicio-torre.js

--- rv/ejercicio-torre.js
+++ rv/ejercicio-torre.js
@@ -1,7 +1,7 @@
 var troncoForma = new THREE.CylinderGeometry(.5, .5, 2);
 var baseForma = new THREE.CylinderGeometry(.65, .65, .25);
 var subaseForma = new THREE.CylinderGeometry(.85, .85, .25);
-var superiorForma = new THREE.CylinderGeometry(0, .5, .5);
+var superiorForma = new THREE.CylinderGeometry(0, .5, .75);
 superiorForma.translate(0,2,0);
 baseForma.translate(0,.25,0);
 troncoForma.translate(0,1,0);
@@ -19,7 +19,7 @@ var torreMalla = new THREE.Mesh(torreForma, material);
 var escena = new THREE.Scene();
 escena.add(torreMalla);
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 10;
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
