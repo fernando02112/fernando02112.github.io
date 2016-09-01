34c0a449c97f04f4d7737e9778e59f227efbd928
fernando02112 <fernando02112@users.noreply.github.com>
2016-09-01 18:53:48 -0500

    Update ejercicio-3.js

--- rv/ejercicio-3.js
+++ rv/ejercicio-3.js
@@ -11,6 +11,8 @@ forma.vertices.push(  new THREE.Vector3(  -1,  1,  1));<!--vértice 7-->
 
 forma.faces.push( new THREE.Face3(3,  2,  1));<!--cara0-->
 forma.faces.push( new THREE.Face3(3,  1,  0));
+forma.faces.push( new THREE.Face3(3,  0,  4));
+
 
 
 forma.computeBoundingSphere();<!--esfera de menor tamaño que acota al objeto-->
