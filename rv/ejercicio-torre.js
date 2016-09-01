c7f24f2881ccf132c67cd48a3b27d2f1d34bc87b
fernando02112 <fernando02112@users.noreply.github.com>
2016-09-01 14:58:00 -0500

    Update ejercicio-torre.js

--- rv/ejercicio-torre.js
+++ rv/ejercicio-torre.js
@@ -1,14 +1,18 @@
 var troncoForma = new THREE.CylinderGeometry(.5, .5, 2);
 var baseForma = new THREE.CylinderGeometry(.65, .65, .25);
+var subaseForma = new THREE.CylinderGeometry(.7, .7, .25);
+baseForma.translate(0,.25,0)
 troncoForma.translate(0,1,0);
 
 var troncoMalla = new THREE.Mesh(troncoForma);
 var baseMalla = new THREE.Mesh(baseForma);
+var subaseMalla = new THREE.Mesh(subaseForma);
 
 var torreForma = new THREE.Geometry();
 
 torreForma.merge(troncoMalla.geometry, troncoMalla.matrix);
 torreForma.merge(baseMalla.geometry, baseMalla.matrix);
+torreForma.merge(subaseMalla.geometry, subaseMalla.matrix);
 
 var material = new THREE.MeshNormalMaterial();
 var torreMalla = new THREE.Mesh(torreForma, material);
