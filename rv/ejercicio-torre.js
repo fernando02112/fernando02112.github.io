d68eac42fb0788d0da1674c12e8eaa0fa4cdefb7
fernando02112 <fernando02112@users.noreply.github.com>
2016-09-01 15:04:00 -0500

    Update ejercicio-torre.js

--- rv/ejercicio-torre.js
+++ rv/ejercicio-torre.js
@@ -25,7 +25,7 @@ var escena = new THREE.Scene();
 escena.add(torreMalla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 10;
 
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
