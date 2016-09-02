b750fbc4f36c54fa57ee034e0b5081a54de567d8
fernando02112 <fernando02112@users.noreply.github.com>
2016-09-01 23:55:53 -0500

    Update ejercicio-4.js

--- rv/ejercicio-4.js
+++ rv/ejercicio-4.js
@@ -19,7 +19,7 @@ var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 50;
+camara.position.z = 30;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
