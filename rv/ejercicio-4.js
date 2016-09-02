bafd8498120ec342c947fe85a81dfabb60bae37f
fernando02112 <fernando02112@users.noreply.github.com>
2016-09-01 23:21:08 -0500

    Create ejercicio-4.js

---
+++ rv/ejercicio-4.js
@@ -0,0 +1,20 @@
+var figura = new THREE.Shape();
+
+figura.moveTo(20,20);
+figura.lineTo(21,22);
+figura.lineTo(21,20);
+figura.lineTo(20,10);
+
+var forma = new THREE. ShapeGeometry(figura);
+var malla = new THREE.Mesh(forma);  
+
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 100;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
