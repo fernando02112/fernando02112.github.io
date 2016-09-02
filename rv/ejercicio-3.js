c1578cbb3a48dfa65bbe16241255942972f79f33
fernando02112 <fernando02112@users.noreply.github.com>
2016-09-01 20:27:05 -0500

    Update ejercicio-3.js

--- rv/ejercicio-3.js
+++ rv/ejercicio-3.js
@@ -1,43 +1,28 @@
 var forma = new THREE.Geometry();<!--primero se define el objeto-->
 forma.vertices.push(  new THREE.Vector3(  1,  0,  1));<!--vertice0-->
-forma.vertices.push(  new THREE.Vector3(  1,  0,  -1));
-forma.vertices.push(  new THREE.Vector3(  -1, 0,  -1));
-forma.vertices.push(  new THREE.Vector3(  -1, 0,  1));
+forma.vertices.push(  new THREE.Vector3(  1,  0,  -1));<!--vértice1-->
+forma.vertices.push(  new THREE.Vector3(  -1, 0,  -1));<!--vértice2-->
+forma.vertices.push(  new THREE.Vector3(  -1, 0,  1));<!--vértice3-->
 forma.vertices.push(  new THREE.Vector3(  1,  1,  1));<!--vertice4-->
-forma.vertices.push(  new THREE.Vector3(  1,  1,  -1));
-forma.vertices.push(  new THREE.Vector3(  -1,  1,  -1));
-forma.vertices.push(  new THREE.Vector3(  -1,  1,  1));<!--vértice 7-->
+forma.vertices.push(  new THREE.Vector3(  1,  1,  -1));<!--vértice5-->
+forma.vertices.push(  new THREE.Vector3(  -1,  1,  -1));<!--vértice6-->
+forma.vertices.push(  new THREE.Vector3(  -1,  1,  1));<!--vértice7-->
 
 
 forma.faces.push( new THREE.Face3(3,  2,  1));<!--cara0-->
-forma.faces.push( new THREE.Face3(3,  1,  0));
-forma.faces.push( new THREE.Face3(3,  0,  4));
-forma.faces.push( new THREE.Face3(0,  1,  4));
-forma.faces.push( new THREE.Face3(1,  4,  5));
+forma.faces.push( new THREE.Face3(3,  1,  0));<!--cara1-->
+forma.faces.push( new THREE.Face3(3,  0,  4));<!--cara2-->
+forma.faces.push( new THREE.Face3(0,  1,  4));<!--cara3-->
+forma.faces.push( new THREE.Face3(1,  4,  5));<!--cara4-->
 forma.faces.push( new THREE.Face3(2,  1,  5));<!--cara5-->
-forma.faces.push( new THREE.Face3(5,  6,  1));
-forma.faces.push( new THREE.Face3(6,  2,  1));
-forma.faces.push( new THREE.Face3(2,  3,  7));
-forma.faces.push( new THREE.Face3(6,  7,  2));
-forma.faces.push( new THREE.Face3(7,  4,  3));
-forma.faces.push( new THREE.Face3(4,  6,  7));
-forma.faces.push( new THREE.Face3(4,  7,  3));<!--cara5-->
-forma.faces.push( new THREE.Face3(4,  5,  6));<!--cara5-->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+forma.faces.push( new THREE.Face3(5,  6,  1));<!--cara6-->
+forma.faces.push( new THREE.Face3(6,  2,  1));<!--cara7-->
+forma.faces.push( new THREE.Face3(2,  3,  7));<!--cara8-->
+forma.faces.push( new THREE.Face3(6,  7,  2));<!--cara9-->
+forma.faces.push( new THREE.Face3(7,  4,  3));<!--cara10-->
+forma.faces.push( new THREE.Face3(4,  6,  7));<!--cara11-->
+forma.faces.push( new THREE.Face3(4,  7,  3));<!--cara12-->
+forma.faces.push( new THREE.Face3(4,  5,  6));<!--cara13-->
 
 forma.computeBoundingSphere();<!--esfera de menor tamaño que acota al objeto-->
 forma.computeFaceNormals();<!--se computan las normales de las caras por el tipo de material a usar-->
