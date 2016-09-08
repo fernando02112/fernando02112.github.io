aca7a55b1d3947dcb4cf8c7e97f88220a58a0ef5
fernando02112 <fernando02112@users.noreply.github.com>
2016-09-08 15:15:07 -0500

    Update ejercicio-tablero.js

--- rv/ejercicio-tablero.js
+++ rv/ejercicio-tablero.js
@@ -1,1 +1,4 @@
-
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+var mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
