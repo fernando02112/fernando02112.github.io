52baf46de2c7aab07387aceabc1dcf618634e0bc
fernando02112 <fernando02112@users.noreply.github.com>
2016-09-13 15:30:54 -0500

    Create ejemplo-luzsinsombras.js

---
+++ rv/ejemplo-luzsinsombras.js
@@ -0,0 +1,23 @@
+
+var iluminacion = new THREE.AmbientLight(0xFFFFFF);
+iluminacio.position.y = 20;
+
+var forma = new THREE.SphereGeometry(1);
+var material = new THREE.MeshLambertMaterial({color: "#00cc00"});
+var malla = new THREE.Mesh(forma, material);
+malla.position.y =2;
+
+var base = new THREE.Mesh(new THREE.BoxGeometry(5, .1, 5), new THREE.MeshLambertMaterial({color: 0xFFFFFF}));
+
+var escena = new THREE.Scene();
+escena.add(malla);
+escena.add(base);
+escena.add(iluminacion);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 15;
+camara.position.y = 5;
+
+var lienzo = document.getElementById("luzSinSombras");
+var renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+
