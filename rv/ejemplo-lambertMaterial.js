d1eb29b6b533e3b0b93b172f5f761203fea79f9a
fernando02112 <fernando02112@users.noreply.github.com>
2016-09-15 12:49:40 -0500

    Update ejemplo-lambertMaterial.js

--- rv/ejemplo-lambertMaterial.js
+++ rv/ejemplo-lambertMaterial.js
@@ -18,4 +18,5 @@ var lienzo = document.getElementById("luzPuntual");
 var renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
 
 renderizador.setSize(600, 600);
+
 renderizador.render(escena, camara);
