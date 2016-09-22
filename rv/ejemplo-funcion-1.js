0114b74bbf3ece692696b0205e2b1a544caaf93c
fernando02112 <fernando02112@users.noreply.github.com>
2016-09-22 14:56:01 -0500

    Create ejemplo-funcion-1.js

---
+++ rv/ejemplo-funcion-1.js
@@ -0,0 +1,14 @@
+function init(p){
+var malla(new THREE.BoxGeometry(p,p,p), new THREE.MeshNormalMaterial());
+
+escena = new THREE.Scene();
+escena.add(malla);
+
+camara = new THREE.PerspectiveCamera();
+renderizador = new THREE.WebGLRender();
+renderizador.setSize(700, 700);
+}
+
+var main = function(p){ <!--function(p) es una fucnión anónima que se puede asignar a un símbolo-->
+renderizador.render(escena, camara);
+}
