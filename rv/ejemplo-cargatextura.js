6fabd6635fc554364d59f95033ef46b01b662325
fernando02112 <fernando02112@users.noreply.github.com>
2016-09-27 15:40:31 -0500

    Update ejemplo-cargatextura.js

--- rv/ejemplo-cargatextura.js
+++ rv/ejemplo-cargatextura.js
@@ -6,6 +6,7 @@ TEXTURA.escena.add(TEXTURA.malla);
 }
 
 TEXTURA.setup = function(){
+ var cargador = new THREE.TextureLoader();
 cargador.load("earth_atmos_2048.jpg", TEXTURA.retrollamada);
 TEXTURA.camara = new THREE.PerspectiveCamera(75, innerWidth/windowHeight, 0.1, 1000);
 TEXTURA.camara.position.z = 5;
