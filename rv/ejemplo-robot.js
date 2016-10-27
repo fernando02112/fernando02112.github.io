198adb2e088bac625037a6879712f74ec1a7737f
fernando02112 <fernando02112@users.noreply.github.com>
2016-10-27 15:46:24 -0500

    Update ejemplo-robot.js

--- rv/ejemplo-robot.js
+++ rv/ejemplo-robot.js
@@ -8,4 +8,145 @@
 }
 Wall.prototype = new THREE.Mesh();
 
-Environment
+Environment.prototype.setMap = function(map) {
+   var _offset = Math.floor(map.length/2);
+ 
+  for(var i = 0; i < map.length; i++)
+   for(var j = 0; j < map.length; j++) {
+    if (map[i][j] === "x")
+     this.add(new wall(1, j - _offset, -(i - _offset)));
+     else if (map[i][j] === "r")
+      this.add( new Robot (0.5, j -_offset, -(i - _offset)));
+   }
+}
+
+function setup() {
+  var mapa = new Array();
+ mapa[0] ="xxxxxxxxxxxxxxxxxxxxxxxxx";
+ mapa[1] ="xr               r      x";
+ mapa[2] ="x                       x";
+ mapa[3] ="x                       x";
+ mapa[4] ="x                       x";
+ mapa[5] ="x                       x";
+ mapa[6] ="x                       x";
+ mapa[7] ="x                       x";
+ mapa[8] ="xxxx  xxxxxxxxxxxxxxxxxxx";
+ mapa[9] ="x                       x";
+ mapa[10]="x     r                 x";
+ mapa[11]="xxxxxxx  r   xxxxxxxxxxxx";
+ mapa[12]="x                       x";
+ mapa[13]="xxx                 xxxxx";
+ mapa[14]="x      r                x";
+ mapa[15]="x                       x";
+ mapa[16]="x                       x";
+ mapa[17]="x                       x";
+ mapa[18]="x                       x";
+ mapa[19]="xxxxxxxxxx     xxxxxxxxxx";
+ mapa[20]="x                       x";
+ mapa[21]="x                       x";
+ mapa[22]="x                       x";
+ mapa[23]="x                       x";
+ mapa[24]="xxxxxxxxxxxxxxxxxxxxxxxxx";
+ 
+ environment = new Environment();
+ 
+ environment.setMap(mapa);
+ 
+ camera = new THREE.PerspectiveCamera();
+ camera.position.z = 30;
+ 
+ renderer = new THREE.WebGLRenderer();
+ renderer.setSize(window.innerHeight*.95, window.innerHeight*.95)
+ document.body.appendChild( renderer.domElement);
+ 
+ environment.add( camera);
+}
+
+function loop() {
+  requestAnimationFrame( loop );
+ 
+  environment.sense();
+  environment.plan();
+  environment.act();
+ 
+ renderer.render(environment, camera);
+ 
+}
+var environment, camera, renderer;
+
+function Sensor( position, direction) {
+   THREE.Raycaster.call(this, position, direction);
+   this.colision = false;
+}
+Sensor.prototype = new THREE.Raycaster();
+
+function Robot(size, x, y){
+   Agent.call(this, x, y);
+ 
+  this.sensor = new Sensor();
+ this.actuator = new THREE.Mesh( new THREE.BoxGeometry(size, size, size), new THREE.MeshBasicMaterial({color: '#aa0000'}));
+ this.actuator.commands = [];
+ this.add(this.actuator);
+}
+Robot.prototype = new Agent();
+
+Robot.prototype.sense = function(environment)  {
+   this.sensor.set(this.position, new THREE.Vector3(Math.cos(this.rotation.z), Math.sin(this.rotation.z), 0));
+  var obstaculo = this.sensor,intersectObjects(environment.children, true);
+ 
+   if ((obstaculo.length > 0 && (obstaculo[0].distance <= .5)))
+    this.sensor.colision = true;
+ else 
+  this.sensor.colision = false;
+ 
+}
+
+Robot.prototype.plan = function(environment) {
+  this.actuator.commands = [];
+ 
+  if (this.sensor.colision == true)
+   this.actuator.commands.push('rotateCCW');
+  else
+   this.actuator.commands.push('goStraight');
+}
+
+Robot.prototype.act = function(environment) {
+  var command = this.actuator.commands.pop();
+ 
+   if (command === undefined)
+    console.log('Undefined command');
+  else if (command in this.operations)
+    this.operations[command](this);
+  else
+   console.log('Unknow command');
+}
+
+
+Robot.prototype.operations = {};
+
+Robot.prototype.operations.goStraight = function(robot, distance) {
+  if (distance === undefined)
+   distance = .05;
+   robot.position.x += distance*Math.cos(robot.rotation.z);
+   robot.position.y += distance*Math.sin(robot.rotation.z);
+}
+
+Robot.prototype.operations.rotateCW = function(robot, angle){
+  if (angle === undefined)
+    angle = -Math.PI/2;
+    robot.rotation.z += angle;
+}
+
+Robot.prototype.operations.rotateCCW = function(robot, angle) {
+  if(angle === undefined)
+   angle = Math.PI/2;
+   robot.rotation.z += angle;
+}
+
+setup();
+loop();
+
+
+
+
+ 
