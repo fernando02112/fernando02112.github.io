77c2af5d31e6700a97567d4bddced3ba074a6ff8
fernando02112 <fernando02112@users.noreply.github.com>
2016-10-27 14:49:02 -0500

    Create ejemplo-robot.js

---
+++ rv/ejemplo-robot.js
@@ -0,0 +1,11 @@
+//Robot
+ function Wall(size, x, y)
+ {
+    THREE.Mesh.call(this, new THREE.BoxGeometry(  size, size, size), new THREE.MeshNormalMaterial());
+    this.size = size;
+    this.position.x = x;
+    this.position.y = y;
+}
+Wall.prototype = new THREE.Mesh();
+
+Environment
