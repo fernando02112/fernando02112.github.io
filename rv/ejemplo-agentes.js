09c8a163f038f02831531cb35a449aa073d6f689
fernando02112 <fernando02112@users.noreply.github.com>
2016-11-03 15:29:19 -0600

    Update ejemplo-agentes.js

--- rv/ejemplo-agentes.js
+++ rv/ejemplo-agentes.js
@@ -58,7 +58,7 @@ Environment.prototype.act = function() {
     this.sensor.set(this.position, new THREE.Vector3(-1, 0, 0) );
     var obstaculo2 = this.sensor.intersectObjects(enviroment.children, true);
     
-  if ((obstaculo1.length > 0 && (obstaculo1[0].distance <= this.radius)) || (obstaculo2.length > 0 && (obstaculo2[0].distance <= this.radius))
+  if ((obstaculo1.length > 0 && (obstaculo1[0].distance <= this.radius)) || (obstaculo2.length > 0 && (obstaculo2[0].distance <= this.radius)))
     this.colision = 1;
    else
     this.colision = 0;
