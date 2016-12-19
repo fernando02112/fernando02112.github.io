b23be4b8b85ba0447dc02142d0f552cf3971422d
fernando02112 <fernando02112@users.noreply.github.com>
2016-12-19 04:31:07 -0600

    Update ejercicio-6.js

--- rv/ejercicio-6.js
+++ rv/ejercicio-6.js
@@ -8,7 +8,7 @@ var planoLejano=1000;
 var camara=new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
 camara.position.z = 200;
 camara.position.y = 25;
-camara.position.x = 50;
+camara.position.x = 0;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.85,
